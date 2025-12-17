962c51f9c5d2b05df5215211023765ff3db2580b
ubuntu <gabriele.deri@gmail.com>
2025-12-17 12:47:02 +0100

    Added gitignore and updated requirements

--- ML_CUP/ml_cup_data_loader.py
+++ ML_CUP/ml_cup_data_loader.py
@@ -1,3 +1,5 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from numpy import loadtxt
 import pandas as pd
 import numpy as np
@@ -21,9 +23,14 @@ def load_training_set():
     # convert to dataframe
     training_set = pd.DataFrame(training_rows, columns=column_names)
 
-    return training_set
+    X = training_set.iloc[:, :-4]
+    y = training_set.iloc[:, -4:]
 
-def load_test_set():
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, train_size = 0.8)
+
+    return X_train, X_test, y_train, y_test
+
+def load_blind_set():
     # laod 16 columns from csv with delimiter ','
     filename = BASE_DIR + "/dataset/ML-CUP25-TS.csv"
     test_rows = loadtxt(filename, delimiter=',', usecols=range(1, 13), dtype=np.float32)
@@ -38,3 +45,21 @@ def load_test_set():
     test_set = pd.DataFrame(test_rows, columns=column_names)
 
     return test_set
+
+def scale_data(X_train, X_test, X_blind, y_train, y_test):
+    
+    feature_scaler = StandardScaler()
+    target_scaler = StandardScaler()
+    
+    # scale inputs
+    X_train_scaled = feature_scaler.fit_transform(X_train)
+    X_test_scaled = feature_scaler.transform(X_test)
+    X_blind_scaled = feature_scaler.transform(X_blind)
+    
+    # scale targets
+    y_train_scaled = target_scaler.fit_transform(y_train)
+    y_test_scaled  = target_scaler.transform(y_test)
+    
+    return (X_train_scaled, X_test_scaled, X_blind_scaled,
+            y_train_scaled, y_test_scaled,
+            feature_scaler, target_scaler)
